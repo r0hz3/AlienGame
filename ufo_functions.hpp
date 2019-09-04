8c5b8321de60b327f901bce246e0c6ca46686097
Kyle Evans <50033258+r0hz3@users.noreply.github.com>
2019-09-04 13:55:46 -0700

    Create ufo_functions.hpp
    Culmination of headers used

---
+++ ufo_functions.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+void display_misses(int misses);
+// Declare functions
+void greet();
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
